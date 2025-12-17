eed53428cc29c3f9effdc2675e87e5e8503d3d05
s2vngwxx <ph1losophe@naver.com>
2025-12-17 20:10:32 +0900

    fix: yield 부분 오타 수정

--- SW/streamlit_server.py
+++ SW/streamlit_server.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from langchain_core.messages import HumanMessage
-from chatbots import app  # chatbots.py에서 app을 가져옵니다.
-import time  # 타이핑 효과를 위한 time 모듈 import
+from chatbots import agents
+import time
 
 st.set_page_config(page_title="가맹점 컨설팅 챗봇", layout="centered")
 st.title("💬 프랜차이즈 컨설팅 챗봇")
@@ -14,7 +14,9 @@ for msg in st.session_state.messages:
     with st.chat_message(msg["role"]):
         st.markdown(msg["content"])
 
-user_input = st.chat_input("질문을 입력하세요.")
+user_input = st.chat_input(
+    "가맹점 구분 번호를 포함하여 데이터 분석 및 마케팅 전략 제안을 요청해보세요. "
+)
 if user_input:
     st.session_state.messages.append({"role": "user", "content": user_input})
     with st.chat_message("user"):
@@ -31,19 +33,19 @@ if user_input:
 
     with st.chat_message("assistant"):
 
-        with st.spinner("컨설팅 AI가 분석 및 전략을 수립 중입니다..."):
-            final_state = app.invoke(state)
+        with st.spinner("에이전트 AI가 분석 및 전략을 수립 중입니다..."):
+            final_state = agents.invoke(state)
 
         final_answer_text = final_state.get(
             "final_answer", "최종 답변을 생성하는 데 실패했습니다."
         )
 
         def stream_text_generator(text_string: str):
-            """문자열을 받아, 단어별로 time.sleep을 주며 yield하는 생성기"""
+            """문자열을 받아 단어별로 time.sleep을 주며 yield하는 생성기"""
 
             for line in text_string.split("\n"):
                 for word in line.split(" "):
-                    yield word + 
+                    yield word + " "
                     time.sleep(0.03)
                 yield "\n"
 
